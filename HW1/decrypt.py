396e950120fc1af11170d8c600b2a6db67cb44f4
Brandon0423 <yy898907@gmail.com>
2022-03-22 00:06:01 +0800

    last push for decrypt

--- HW1/decrypt.py
+++ HW1/decrypt.py
@@ -1,43 +1,40 @@
-from itertools import count
-from posixpath import split
-import numpy as np
 import sys
-
-from numpy import ndenumerate, outer
+import numpy as np
 from sklearn import exceptions
 
 
 # KEY正向左 負向右 (解密) A65 90 a97 122
-
-
-def Caesar(ciphertxt, key):
+def caesar(ciphertxt, key):
     output = ""
     try:
-        key = int(key) % 26
+        ikey = int(key) % 26
     except Exception as e:
         raise SystemExit(
             f"Caesar key is invalid: {key} ") from e
+
     for w in ciphertxt:
-        if (w >= 'A' and w <= 'Z'):
-            tmp = (ord(w)-key)
-            if tmp > 90:
-                tmp = tmp - 26
-            elif tmp < 65:
-                tmp = tmp + 26
-            output = output + chr(tmp)
-        elif (w >= 'a' and w <= 'z'):
-            tmp = (ord(w)-key)
-            if tmp > 122:
-                tmp = tmp - 26
-            elif tmp < 97:
-                tmp = tmp + 26
-            output = output + chr(tmp)
+        if w.isalpha():
+            num = ord(w)
+            num = num - ikey
+
+            if w.isupper():
+                if num > ord('Z'):
+                    num = num - 26
+                elif num < ord('A'):
+                    num = num + 26
+
+            if w.islower():
+                if num > ord('z'):
+                    num = num - 26
+                elif num < ord('a'):
+                    num = num + 26
+            output = output + chr(num)
         else:
             output = output + w
-    print(output.lower())
+    print(output)
 
 
-def Playfair(ciphertxt, key):
+def playfair(ciphertxt, key):
 
     # 1. preprocess and try exception
     ciphertxt = ciphertxt.replace(" ", "").upper()
@@ -46,7 +43,7 @@ def Playfair(ciphertxt, key):
             raise SystemExit(
                 f"ciphertxt is invalid with the char: {x}")
 
-    # 2.optional: create key table
+    # 2. create key table
     AtoZ = ''.join(chr(x) for x in range(65, 91))
     tmp = key.replace(" ", "").upper()+AtoZ
     keytable = ''
@@ -94,56 +91,43 @@ def Playfair(ciphertxt, key):
         output = output + keytable[preRow*5 + preCol]
         output = output + keytable[postRow*5 + postCol]
 
-    print(output.lower())
+    print(output)
 
 
-def Vernam(ciphertxt, key):
+def vernam(ciphertxt, key):
+    ciphertxt = ciphertxt.upper()
+    if len(key) < len(ciphertxt):
+        ikey = key + ciphertxt[0:len(ciphertxt)-len(key)]
 
-    # 1. preprocess and check
-    ciphertxt = ciphertxt.replace(" ", "").upper()
-    for x in ciphertxt:
-        if not x.isalpha():
-            raise SystemExit(
-                f"ciphertxt is invalid with the char: {x}")
-
-    # 2. decrypt
-    ikey = key.replace(" ", "").upper()
     output = ""
     for i in range(0, len(ciphertxt)):
         # xor key and ciphertxt
         tmp = ((ord(ikey[i])-ord('A')) ^
-               (ord(ciphertxt[i])-ord('A')))+ord('A')
+               (ord(ciphertxt[i])-ord('A'))) + ord('A')
+        # print(tmp)
         output = output + chr(tmp)
-        ikey = ikey + chr(tmp)
     # print(ikey)
     # print(ciphertxt)
-    print(output.lower())
+    print(output)
 
 
-def Railfence(ciphertxt, key):
+def railfence(ciphertxt, key):
     # 1. preprocess and check
-    # ciphertxt = ciphertxt.replace(" ", "").upper()
-    # for x in ciphertxt:
-    #     if not x.isalpha() and x != ' ' and x != ',' and x != '.':
-    #         raise SystemExit(
-    #             f"ciphertxt is invalid with the char: {x}")
+    ciphertxt = ciphertxt.replace(" ", "")
     if key.isdigit():
         key = int(key)
     else:
         raise SystemExit(
-            f"key is invalid with the char: {key}")
+            f"key: {key} is invalid")
 
     # split string
-     # len(str) 皆可表示為 n*(2k-2)+餘
+    # len(str) 皆可表示為 n*(2k-2)+餘
     # \/\/ 理想整除 第一 中間 最後，切割字串的個數為 [n,2n,n]
-    # \/\, 上殘型 [n+1, 2n+1~2n+2, n+1] 中間由前數來共 2k-2-餘個 2n+1
-    # \/\/'下殘型 [n+1, 2n+1~2n+0, n]  中間由前數來 共 餘-1個 2n+1
+    # \/\, 上殘型 [n+1, 2n+1~2n+2, n+1] 中間由前數來共 (2k-2-餘) 個 2n+1
+    # \/\/'下殘型 [n+1, 2n+1~2n+0, n]  中間由前數來 共 (餘-1) 個 2n+1
     txtlen = len(ciphertxt)
-    txtquo, txtmod = int(txtlen / (2 * key - 2)), txtlen % (2 * key - 2)
-    txtTable = {}
-    splitIndex = 0
+    txtquo, txtmod = int(txtlen / (2 * key - 2)), txtlen % (2 * key - 2)  # n,餘
     # idea-
-    print(f"{txtlen}%{(2 * key - 2)} = {txtmod}")
     if txtmod == 0:
         _first, _mid, _last = (txtquo, txtquo*2, txtquo)
         modCount = 0
@@ -151,15 +135,15 @@ def Railfence(ciphertxt, key):
     # 上殘
     elif txtmod >= key:
         _first, _mid, _last = (txtquo+1, txtquo*2+2, txtquo+1)
-        modCount = 2*key - 2 - txtmod  # 1
+        modCount = 2*key - 2 - txtmod
         modPlus = -1
-        print("==========")
     # 下殘
     else:
         _first, _mid, _last = (txtquo+1, txtquo*2, txtquo)
         modCount = txtmod-1
         modPlus = 1
-
+    txtTable = {}
+    splitIndex = 0
     for i in range(0, key):
         if i == 0:
             splitNum = _first
@@ -171,10 +155,8 @@ def Railfence(ciphertxt, key):
                 modCount = modCount-1
             else:
                 splitNum = _mid
-        print(f"modcount:{modCount}, modplus:{modPlus}")
         txtTable[i] = ciphertxt[splitIndex:splitIndex + splitNum]
         splitIndex = splitIndex + splitNum
-        print(txtTable[i])
 
     # decrypt
     output = ''
@@ -192,7 +174,7 @@ def Railfence(ciphertxt, key):
     print(output)
 
 
-def RowTransposition(ciphtxt, key):
+def row(ciphtxt, key):
     # preprocess
 
     chrNum = int(len(ciphertxt)/len(key))
@@ -203,7 +185,7 @@ def RowTransposition(ciphtxt, key):
     splitCount = 0
     keySorted = ''.join(sorted(key))
     for i in range(0, len(key)):
-        # key 較前序的 在有餘數時 會多出1個字母
+        # key 較前序的(共餘數個) 會多分1個字母
         if key.index(keySorted[i]) < txtMod:
             ltModNum = 1
         else:
@@ -213,8 +195,8 @@ def RowTransposition(ciphtxt, key):
     # print(splitTxt)
 
     output = ''
-    for i in key:
-        print(splitTxt[i])
+    # for i in key:
+    # print(splitTxt[i])
     for num in range(0, chrNum+1):
         for i in key:
             tmp = splitTxt[i]
@@ -225,21 +207,24 @@ def RowTransposition(ciphtxt, key):
 
 # py program.py -m .. -i .. -k ..
 try:
+    # print(sys.argv)
     mindex = sys.argv[1:].index('-m')
     iindex = sys.argv[1:].index('-i')
     kindex = sys.argv[1:].index('-k')
 
 except Exception as e:
     raise SystemExit(
-        f"Usage: {sys.argv[0]} -m method -i ciphertxt -k key :)") from e
+        f"Usage: {sys.argv[0]} -m method -i ciphertxt -k key, check command :)") from e
 
 method = sys.argv[mindex+2]
 ciphertxt = ' '.join(sys.argv[iindex+2:kindex+1])
 key = ' '.join(sys.argv[kindex+2:])
 
 # globals()[method](ciphertxt, key)
-# try:
+
+
+if method != 'caesar' and method != 'playfair' and method != 'vernam' and method != 'railfence' and method != 'row':
+    raise SystemExit(f"method: {method} is non-defined, accepted method: caesar,"
+                     'playfair, vernam, railfence, row')
+
 globals()[method](ciphertxt, key)
-# except Exception as e:
-#     raise SystemExit(f"method: {method} is non-defined, accepted method: Caesar,"
-#                      'Playfair, Vernam, RailFence, RowTransposition') from e
